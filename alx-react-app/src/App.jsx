30c234c8dde01e556acefdd711c202963a461691
Qhama Hani <qhamahani@gmail.com>
2026-01-14 04:29:51 +0200

    added components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,15 @@
 import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./Header";
+import MainContent from "./MainContent";
+import Footer from "./Footer";
 function App() {
-  return <WelcomeMessage />;
+  return (
+    <>
+      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  );
 }
+export default App;
---
+++ alx-react-app/src/components/Header.jsx
@@ -0,0 +1,6 @@
+function Header() {
+  <header>
+    <h1>My Favorite Cities</h1>
+  </header>;
+}
+export default Header;
