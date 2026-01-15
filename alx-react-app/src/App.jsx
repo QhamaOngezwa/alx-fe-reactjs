3cde2ded1a0db0777dce66dabf61d81c6fc6448a
Qhama Hani <qhamahani@gmail.com>
2026-01-15 15:18:44 +0200

    First component with props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import WelcomeMessage from "./components/WelcomeMessage";
 import Header from "./Header";
 import MainContent from "./MainContent";
 import Footer from "./Footer";
+import UserProfile from "./components/UserProfile";
 function App() {
   return (
     <>
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <UserProfile name="Qhama" age="26" bio="Creative enthusiast" />
     </>
   );
 }
