0a1413562cfa73cd20e78af3d7397284f352fe6f
Qhama Hani <qhamahani@gmail.com>
2026-01-18 20:44:13 +0200

    add user profile

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,9 +1,13 @@
-const UserProfile = (props) => {
+import React from "react";
+import UserContext from "./UserContext";
+
+const UserProfile = (userData) => {
+  const userData = React.useContext(UserContext);
   return (
     <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
+      <h2>{userData.name}</h2>
+      <p>Age: {userData.age}</p>
+      <p>Bio: {userData.bio}</p>
     </div>
   );
 };
