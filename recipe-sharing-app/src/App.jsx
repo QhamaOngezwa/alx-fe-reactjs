6782c1c5effeec691aaf54d38a76b717dd392126
Qhama Hani <qhamahani@gmail.com>
2026-01-25 19:53:43 +0200

    Add favorites and recommendations list

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -5,6 +5,8 @@ import './App.css'
 import RecipeList from './components/RecipeList'
 import AddRecipeForm from './components/AddRecipeForm'
 import { Routes, Route } from "react-router-dom";
+import FavoritesList from './components/FavoritesList'
+import RecommendationsList from './components/RecommendationsList'
 
 function App() {
   const [count, setCount] = useState(0)
