49d7c78deb279c724e8d3d93081f24f1c0da851a
Qhama Hani <qhamahani@gmail.com>
2026-01-25 19:27:59 +0200

    import route

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import RecipeList from './components/RecipeList'
 import AddRecipeForm from './components/AddRecipeForm'
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
