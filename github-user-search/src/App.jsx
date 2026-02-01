6566681d827e3b0f8206a8bcc758cba661a89716
Qhama Hani <qhamahani@gmail.com>
2026-02-01 22:56:17 +0200

    import github-service component

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import UserInformation from "./components/UserInformation";
+import githubService from "./services/githubService";
 
 function App() {
   const [count, setCount] = useState(0);
