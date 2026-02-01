4b73299174c390c5d46c0124dbd2feb54107cbcd
Qhama Hani <qhamahani@gmail.com>
2026-02-01 22:48:38 +0200

    api call

---
+++ github-user-search/src/services/githubService.js
@@ -0,0 +1,27 @@
+function fetchUserData(username) {
+  const apiUrl = `https://api.github.com/users/${username}`;
+  return fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      return {
+        login: data.login,
+        name: data.name,
+        avatarUrl: data.avatar_url,
+        bio: data.bio,
+        publicRepos: data.public_repos,
+        followers: data.followers,
+        following: data.following,
+        createdAt: data.created_at,
+      };
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+      throw error;
+    });
+}
+export { fetchUserData };
