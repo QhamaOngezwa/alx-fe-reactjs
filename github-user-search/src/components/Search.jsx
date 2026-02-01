bdd0adb284bf23a86f48ab125c640a0f5b56367b
Qhama Hani <qhamahani@gmail.com>
2026-02-01 22:04:56 +0200

    implement state handling

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,3 +1,35 @@
+import { useState } from "react";
+
 function Search() {
-  return <div>Search Component</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ name, email });
+    setName("");
+    setEmail("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Name"
+      />
+
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+      />
+
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
+
+export default Search;
