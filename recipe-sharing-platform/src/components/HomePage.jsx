3a4b5efbf8422f45be99de7047c522808eebb25d
Qhama Hani <qhamahani@gmail.com>
2026-02-22 23:01:49 +0200

    implement recipe card navigation

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,5 +1,6 @@
 import {useState, useEffect} from react
 import data from '../data/data.json'
+import { Link } from 'react-router-dom'
 
 function HomePage() {
   const [recipes, setRecipes] = useState([])
